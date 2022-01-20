75af791ea131be014423fd50f2c3af609dc9d9cd
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-20 22:16:48 +0530

    This is the demo for Const, Var, Let

---
+++ var_demo.js
@@ -0,0 +1,21 @@
+//LET vs VAR vs CONST
+
+//var(The Value assigned using var can be changed)
+var val = 100;
+//let(The Value which is assigned using let can be changed)
+let firstname="Phil";
+//const(In Const we cant change the Value)
+const lastname = "Mark";
+
+
+
+//Example(Let us assume an address of a person)
+var name="Tommy"; //Name can be assigned as var which can be changed depending on the owner
+let zip="583201"; //ZIP code can be considered to be let since the same address can be present in another city too..!!
+const address="#543 4th Main 9th Avenue CA USA" // I have given address as Const becuase to the given zip code that is the only address which is valid it can't be changed..!!
+console.log(val);
+console.log(firstname);
+console.log(lastname);
+console.log(name);
+console.log(zip);
+console.log(address);
