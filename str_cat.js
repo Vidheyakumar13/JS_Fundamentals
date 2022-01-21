fe119199130c5ef3cf076ebaf3ebbdaa0e05d85c
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-21 23:58:36 +0530

    String Concatination
    The Code is been Commented..!! Before trying Uncomment the code

---
+++ str_cat.js
@@ -0,0 +1,24 @@
+// var color1 = 'blue';
+// let color2 = 'red';
+// const color3 = '5';
+
+// var string_cat = color1+color2+color3;
+// console.log(string_cat);
+
+// Concatinating two strings
+
+// var first_name = "Michale"
+// var last_name = "Jackson"
+
+// var fullName= first_name +last_name;
+
+// console.log(fullName);
+
+//concatinating two digits
+
+// var number1 = 10;
+// var number2 = 30;
+
+// var num= number1+number2;
+
+// console.log(num);
