0f909a941a57088a8634297cd019850c472f7ce4
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-24 11:35:25 +0530

    Data Types

---
+++ data_type.js
@@ -0,0 +1,18 @@
+// String
+const text="data types"
+//Number
+const digit=50;
+//Boolean
+let covid=true;
+let happy=false;
+//Null
+let final=null;
+//Undefined
+let firstname;
+
+console.log(typeof text);
+console.log(typeof digit);
+console.log(typeof covid);
+console.log(typeof happy);
+console.log(typeof final);
+console.log(typeof firstname);
