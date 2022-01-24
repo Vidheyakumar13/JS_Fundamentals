658fd128b1ae48431dc1a785f4d7a6037154a52c
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-24 16:52:56 +0530

    Example of an Array

---
+++ arr_app.js
@@ -0,0 +1,21 @@
+// Array 
+// Let us create a array of cricket players
+
+const player1='Virat Kohli'
+const player2='Virat Kohli'
+const player3='Virat Kohli'
+const player4='Virat Kohli'
+const player5='Virat Kohli'
+const player6='Virat Kohli'
+
+//And the list goes on till 11 players
+// By using array we can make an easy way to add delete acess the list of the players
+let players = ['Virat','Rohit','dhawan','rahul']
+//To access any value from the array we can do:
+console.log(players[3]);
+// To replace the value in an array we do:
+let BestPlayer= players[1]
+console.log(BestPlayer)
+players[1]='MS Dhoni';
+console.log(players[1]);
+console.log(players);
