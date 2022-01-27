5a061ca445dc819f59f3c28a2feb952ebd64eb44
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-27 20:39:17 +0530

    Conditional Statements

---
+++ ex_cs_app.js
@@ -0,0 +1,14 @@
+const num1= 15;
+const num2= 15;
+const compareValue= num1>=num2;
+
+if(num1>num2){
+console.log('First Number is greater than Second Number');
+
+}
+else if(compareValue){
+    console.log('First number and second number both are equal');
+}
+else{
+    console.log('Second Number is greater than first Number');
+}
