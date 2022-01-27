c7c2895e0ced250f8285b3599ff970280c59e583
Vidheyakumar13 <95712544+Vidheyakumar13@users.noreply.github.com>
2022-01-27 20:00:43 +0530

    Example for Objects

---
+++ ex_obj_app.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName:'N Vidheya',
+    lastName:'Kumar',
+    age:22,
+    Education:true,
+    Nationality:'India'
+}
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.Education);
+console.log(person.Nationality);
